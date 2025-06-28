68a020137d4c5f7d88354ac72bd177a949470dff
Francesco Velluto <vellfr00@gmail.com>
2025-06-28 12:06:07 +0200

    test-app: add slf4j and java logger strategies

--- spring-uncaught-guard-test-app/src/main/java/com/velluto/uncaughtguardtestapp/UncaughtGuardTestApplication.java
+++ spring-uncaught-guard-test-app/src/main/java/com/velluto/uncaughtguardtestapp/UncaughtGuardTestApplication.java
@@ -1,11 +1,18 @@
 package com.velluto.uncaughtguardtestapp;
 
 import com.velluto.uncaughtguard.annotations.EnableUncaughtGuard;
+import com.velluto.uncaughtguard.strategies.UncaughtGuardJavaLoggerLoggingStrategy;
+import com.velluto.uncaughtguard.strategies.UncaughtGuardSlf4jLoggingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableUncaughtGuard
+@EnableUncaughtGuard(
+        loggingStrategies = {
+                UncaughtGuardSlf4jLoggingStrategy.class,
+                UncaughtGuardJavaLoggerLoggingStrategy.class
+        }
+)
 public class UncaughtGuardTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(UncaughtGuardTestApplication.class, args);
